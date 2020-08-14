02f0d8ff52335c4de953d1c3d5a69fea1b02edcc
A <bing3304@gmail.com>
2020-08-14 15:05:34 -0500

    Add player conditions for animation rigging

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,8 +7,13 @@ public class PlayerController : MonoBehaviour
     public Animator anim;
     public Rigidbody2D rb;
 
-
     public int jumpForce;
+
+    public Transform groundPoint;
+
+    public LayerMask groundLayer;
+
+    public bool grounded;
 
     // Start is called before the first frame update
     void Start()
@@ -16,13 +21,20 @@ public class PlayerController : MonoBehaviour
         
     }
 
+    private void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapPoint(groundPoint.position,groundLayer);
+    }
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(grounded && Input.GetKeyDown(KeyCode.Space))
         {
             anim.SetTrigger("Jump");
             rb.AddForce(Vector2.up * jumpForce);
         }
+
+        anim.SetFloat("yVelocity", rb.velocity.y);
+        anim.SetBool("Grounded",grounded);
     }
 }
